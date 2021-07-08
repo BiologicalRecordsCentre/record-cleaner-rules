5b15369a903afb8e307d66f39852c0d8efe0d4c0
Jim Bacon <jame2@ceh.ac.uk>
2021-07-08 11:54:41 +0100

    Rebuild zip files
    Paths within zip files were wrong.
    Add script to ensure they are built correctly each time.
    Relocated for clearer organisation.

---
+++ rules/Butterfly Conservation/package.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+cd BC_IDifficulty
+zip -r ../../../zip/BC/BC_IDifficulty.zip BC_IDifficulty.txt
+cd ../"BC Butterfly rules"
+zip -r ../../../zip/BC/"BC Butterfly rules.zip" BC
+cd ../NMRS_IDifficulty
+zip -r ../../../zip/BC/NMRS_IDifficulty.zip NMRS_IDifficulty.txt
+cd ../"NMRS Moth rules"
+zip -r ../../../zip/BC/"NMRS Moth rules.zip" NMRS
