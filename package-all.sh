531105ef2b14e1b9bd680d3acf692683320dd4a2
Jim Bacon <jame2@ceh.ac.uk>
2022-03-14 18:33:03 +0000

    batch: Add more schemes to package-all

--- package-all.sh
+++ package-all.sh
@@ -1,4 +1,23 @@
 #! /bin/bash
 
-cd rules/Butterfly Conservation
+cd "rules/British Myriapod and Isopod Group"
 ./package.sh
+cd "../Butterfly Conservation"
+./package.sh
+cd "../Dragonfly Recording Scheme"
+./package.sh
+cd "../Ephemeroptera Recording Scheme"
+./package.sh
+cd "../Ground Beetle Recording Scheme"
+./package.sh
+cd "../Larger Brachycera Recording Scheme"
+./package.sh
+cd "../Orthopteroids of the British Isles Recording Scheme"
+./package.sh
+cd "../Plecoptera Recording Scheme"
+./package.sh
+cd "../Trichoptera Recording Scheme"
+./package.sh
+cd "../UK Ladybird Survey"
+./package.sh
+cd ../..
