25d5e8badf6c5c70755af8df10eebecf124e8583
Jim Bacon <jame2@ceh.ac.uk>
2022-03-17 10:57:34 +0000

    batch: Bind mount rules to docker container
    Rules are not added to docker image now but mounted at run time so
    changes are immediately available.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 # An Apache web server image.
 FROM httpd:2.4
-# To serve the contents of the zip folder.
-COPY ./zip/ /usr/local/apache2/htdocs/
 # Having servers.txt as the default document.
 COPY ./my-httpd.conf /usr/local/apache2/conf/httpd.conf
+# We will mount the rule files when the container is started.
--- serve.sh
+++ serve.sh
@@ -7,6 +7,11 @@ export BASEPATH=http://localhost:8080
 ./package-all.sh
 
 docker build --tag rc-rules-server .
-docker run -d -p 8080:80 --name rc-rules rc-rules-server
+docker run -d \
+  -p 8080:80 \
+  --name rc-rules \
+  --mount type=bind,source="$(pwd)"/zip/,target=/usr/local/apache2/htdocs \
+  rc-rules-server
 
 echo "Rules are now being served at http://localhost:8080"
+echo "If you repackage a rule file it will be immediately available."
