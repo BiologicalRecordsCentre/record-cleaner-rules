79edb762af9e6f53d1a6800480d321fcf728b44a
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2024-03-19 14:42:22 +0000

    Update txt_to_csv.r

--- scripts/txt_to_csv.r
+++ scripts/txt_to_csv.r
@@ -1,25 +1,41 @@
 library(tidyverse)
-scheme_name <- "Trichoptera Recording Scheme"
-rule_folder <- "TRS Caddisfly rules/TRS"
-id_folder <- "TRS_IDifficulty"
-output_name <- "TRS Caddisfly"
 
-setwd(paste("C:/Users/robhut/OneDrive - UKCEH/Indicia/record-cleaner-rules/rules", scheme_name, sep = "/"))
+library(RODBC)
 
+org <- "BC"
+scheme <- "BC"
+
+warehouse <- odbcConnect("PostgreSQL35W")
+
+uksi <- sqlQuery(warehouse, 
+                 
+                 "select distinct cttl.taxon, cttl.search_code as tvk
+from indicia.cache_taxa_taxon_lists cttl
+where taxon_list_id=15"
+                 
+)
+
+close(warehouse)
+
+file_location <- "C:/Users/robhut/OneDrive - UKCEH/Indicia/record-cleaner-rules"
+
+if(dir.exists(paste(file_location, "rules_as_csv", org, scheme, sep = "/" )) == FALSE) {
+  
+  dir.create(paste(file_location, "rules_as_csv", org, scheme, sep = "/" ), recursive = TRUE)
+  
+}
+
+setwd(paste(file_location, "rules", sep = "/"))
 
 ### Grid Reference #####
-files <- paste(rule_folder, "tenkm", sep = "/") 
+files <- paste(org, scheme, "tenkm", sep = "/") 
 fileNames <- list.files(files)
 
 if(length(fileNames) != 0) {
   
-  tenkm_rules <- data.frame(matrix(ncol = 5, nrow = 0))
+  tenkm_rules <- data.frame(matrix(ncol = 1, nrow = 0))
   
-  colnames(tenkm_rules) <- c("rule_group",
-                             "rule_name",
-                             "tvk",
-                             "error_message",
-                             "value")
+  colnames(tenkm_rules) <- c("tvk")
   tenkm_rules <- tenkm_rules %>%
     mutate_all(as.character)
   
@@ -32,34 +48,53 @@ if(length(fileNames) != 0) {
       filter(grepl("=", tenkm, fixed = TRUE)) %>%
       separate(tenkm, into = c("title","value"),sep = "=")%>%
       pivot_wider(names_from = title, values_from = value) %>%
-      rename(tvk = DataRecordId,
-             error_message = ErrorMsg,
-             rule_name = ShortName,
-             rule_group = TestType) %>%
-      select(rule_group, rule_name, tvk, error_message)
+      rename(tvk = DataRecordId) %>%
+      select( tvk)
     values <- temp %>%
       filter(!grepl("=", tenkm, fixed = TRUE),
              !grepl("[", tenkm, fixed = TRUE)) %>%
-      rename(value = tenkm)
-    
+      rename(value = tenkm)%>%
+      mutate(km100 = gsub("..$", "", value),
+             km10 = case_when(nchar(value) == 4 ~ gsub("^..", "", value),
+                              TRUE ~ gsub("^.", "", value))) %>%
+      select(-value) %>%
+      group_by(km100) %>%
+      arrange(km10) %>%
+      mutate(row = paste("n", row_number(), sep = "")) %>%
+      pivot_wider(names_from = row, values_from = km10) %>%
+      unite(km10, -km100, sep = " ", na.rm = TRUE) %>%
+      mutate(coord_system = case_when(nchar(km100) == 1 ~ "OSNI",
+                                      str_detect(km100, "^W") ~ "CI",
+                                      TRUE ~ "OSGB")) %>%
+      arrange(coord_system, km100)
     rule <- bind_cols(metadata, values)
     tenkm_rules <- bind_rows(rule, tenkm_rules)
+  
   }
+  
+  tenkm_rules <- right_join(uksi, tenkm_rules, by = "tvk")
+  
+} else {
+  
+  tenkm_rules <- data.frame(matrix(ncol = 5, nrow = 0))
+  
+  colnames(tenkm_rules) <- c("taxon",
+                             "tvk",
+                             "km100",
+                             "km10",
+                             "coord_system")
   
 }
 
+write.csv(tenkm_rules, paste(file_location, "/rules_as_csv/", org, "/", scheme, "/tenkm.csv", sep = ""), na = "", row.names = FALSE)
 
 ### Recording Period #####
-files <- paste(rule_folder, "period", sep = "/") 
+files <- paste(org, scheme, "period", sep = "/") 
 fileNames <- list.files(files)
 if(length(fileNames) != 0) {
-  period_rules <- data.frame(matrix(ncol = 5, nrow = 0))
+  period_rules <- data.frame(matrix(ncol = 1, nrow = 0))
 
-  colnames(period_rules) <- c("rule_group",
-                         "rule_name",
-                         "tvk",
-                         "error_message",
-                         "value")
+  colnames(period_rules) <- c("tvk")
   period_rules <- period_rules %>%
     mutate_all(as.character)
 
@@ -69,46 +104,67 @@ for(i in 1:length(fileNames)) {
   temp <- read.delim(paste(files, file_name, sep = "/"), stringsAsFactors = FALSE)
   colnames(temp) <- "period"
   rule <- temp %>%
-    filter(grepl("=", period, fixed = TRUE)) %>%
-    separate(period, into = c("title","value"),sep = "=")%>%
+    filter(grepl("=", period, useBytes = TRUE)) %>%
+    separate_wider_delim(period, delim= "=", names = c("title","value")) %>%
+    filter(grepl("tvk|start|end", title, ignore.case = TRUE)) %>%
+    mutate(value = gsub("^ ", "", value)) %>%
     pivot_wider(names_from = title, values_from = value) %>%
-    rename(tvk = Tvk,
-           error_message = ErrorMsg,
-           rule_name = ShortName,
-           rule_group = TestType,
-           start_date = StartDate,
-           end_date = EndDate) %>%
-    mutate(start_date = case_when(nchar(start_date) == 8 ~ paste(gsub("....$", "", start_date),
-                                                                 gsub("..$", "",  gsub("^....", "", start_date)),
-                                                                 gsub("^......", "", start_date), sep = "-"),
-                                  TRUE ~ start_date),
-           end_date = case_when(nchar(end_date) == 8 ~ paste(gsub("....$", "", end_date),
-                                                                 gsub("..$", "",  gsub("^....", "", end_date)),
-                                                                 gsub("^......", "", end_date), sep = "-"),
-                                TRUE ~ end_date),
-           value = paste(start_date, end_date, sep = " to "),
-           value = gsub(" to NA", "", value),
-           value = gsub("NA to ", "", value),
-           value = gsub(" to $", " to present", value)) %>%
-    select(rule_group, rule_name, tvk, error_message, value)
+    janitor::clean_names() %>%
+    mutate(start_day = case_when(nchar(start_date) == 8 ~  gsub("^......", "", start_date)),
+           start_month = case_when(nchar(start_date) == 8 ~ gsub("..$", "",  gsub("^....", "", start_date))),
+           start_year = case_when(nchar(start_date) == 8 ~ gsub("....$", "", start_date)))%>%
+    mutate(end_day = case_when(nchar(end_date) == 8 ~ gsub("^......", "", end_date)),
+           end_month = case_when(nchar(end_date) == 8 ~ gsub("..$", "",  gsub("^....", "", end_date))),
+           end_year = case_when(nchar(end_date) == 8 ~ gsub("....$", "", end_date))) %>%
+    select(tvk, start_day, start_month, start_year, end_day, end_month, end_year)
 
   period_rules <- bind_rows(rule, period_rules)
 }
-
+  
+  period_rules <- right_join(uksi, period_rules, by = "tvk")
+  
+} else {
+  
+  period_rules <- data.frame(matrix(ncol = 8, nrow = 0))
+  
+  colnames(period_rules) <- c("taxon",
+                              "tvk",
+                              "start_day",
+                              "start_month",
+                              "start_year",
+                              "end_day",
+                              "end_month",
+                              "end_year")
+  
 }
-### Seasonal Recording Period #####
 
-files <- paste(rule_folder, "seasonalperiod", sep = "/") 
-fileNames <- list.files(files)
+
+write.csv(period_rules, paste(file_location, "/rules_as_csv/", org, "/", scheme, "/period.csv", sep = ""), na = "", row.names = FALSE)
+
+### Period Within Year #####
+
+files_s <- paste(org, scheme, "seasonalperiod", sep = "/") 
+fileNames_s <- list.files(files_s)
+
+files_f <- paste(org, scheme, "flightperiod", sep = "/") 
+fileNames_f <- list.files(files_f)
+
+if(length(fileNames_s) > length(fileNames_f)) {
+  
+  fileNames <- fileNames_s
+  files <- files_s
+  
+} else { 
+  
+  fileNames <- fileNames_f
+  files <- files_f }
+
+
 if(length(fileNames) != 0) {
-s_period_rules <- data.frame(matrix(ncol = 5, nrow = 0))
+s_period_rules <- data.frame(matrix(ncol = 1, nrow = 0))
 
-colnames(s_period_rules) <- c("rule_group",
-                            "rule_name",
-                            "tvk",
-                            "error_message",
-                            "value")
-s_period_rules <- s_period_rules %>%
+colnames(s_period_rules) <- c("tvk")
+wy_period_rules <- s_period_rules %>%
   mutate_all(as.character)
 
 for(i in 1:length(fileNames)) {
@@ -117,89 +173,52 @@ for(i in 1:length(fileNames)) {
   temp <- read.delim(paste(files, file_name, sep = "/"), stringsAsFactors = FALSE)
   colnames(temp) <- "period"
   rule <- temp %>%
-    filter(grepl("=", period, fixed = TRUE)) %>%
-    separate(period, into = c("title","value"),sep = "=")%>%
+    filter(grepl("=", period, useBytes = TRUE)) %>%
+    separate_wider_delim(period, delim= "=", names = c("title","value")) %>%
+    filter(grepl("tvk|start|end", title, ignore.case = TRUE)) %>%
+    mutate(value = gsub("^ ", "", value)) %>%
     pivot_wider(names_from = title, values_from = value) %>%
-    rename(tvk = Tvk,
-           error_message = ErrorMsg,
-           rule_name = ShortName,
-           rule_group = TestType,
-           start_date = StartDate,
-           end_date = EndDate) %>%
-    mutate(start_date = case_when(nchar(start_date) == 4 ~ paste(gsub("..$", "", start_date),
-                                                                 gsub("^..", "", start_date), sep = "-"),
-                                  TRUE ~ start_date),
-           end_date = case_when(nchar(end_date) == 4 ~ paste(gsub("..$", "", end_date),
-                                                             gsub("^..", "", end_date), sep = "-"),
-                                TRUE ~ end_date),
-           value = paste(start_date, end_date, sep = " to "),
-           value = gsub(" to NA", " to 12-31", value),
-           value = gsub("NA to ", "01-01 to ", value),
-           value = gsub(" to $", " to 12-31", value)) %>%
-    select(rule_group, rule_name, tvk, error_message, value)
+    janitor::clean_names() %>%
+    mutate(start_month = case_when(nchar(start_date) == 4 ~ gsub("..$", "", start_date)),
+           start_day = case_when(nchar(start_date) == 4 ~ gsub("^..", "", start_date)),
+           end_month = case_when(nchar(end_date) == 4 ~ gsub("..$", "", end_date)),
+           end_day = case_when(nchar(end_date) == 4 ~ gsub("^..", "", end_date))) %>%
+    select(tvk, start_day, start_month, end_day, end_month)
   
-  s_period_rules <- bind_rows(rule, s_period_rules)
-}
+  wy_period_rules <- bind_rows(rule, wy_period_rules)
 }
 
-### Flight Period #####
+wy_period_rules <- right_join(uksi, wy_period_rules, by = "tvk") %>%
+  mutate(stage = "Adult")
 
-files <- paste(rule_folder, "flightperiod", sep = "/") 
-fileNames <- list.files(files)
-if(length(fileNames) != 0) {
-  f_period_rules <- data.frame(matrix(ncol = 5, nrow = 0))
+} else {
   
-  colnames(f_period_rules) <- c("rule_group",
-                                "rule_name",
+  s_period_rules <- data.frame(matrix(ncol = 7, nrow = 0))
+  
+  colnames(s_period_rules) <- c("taxon",
                                 "tvk",
-                                "error_message",
-                                "value")
-  f_period_rules <- f_period_rules %>%
+                                "start_day",
+                                "start_month",
+                                "end_day",
+                                "end_month",
+                                "stage")
+  wy_period_rules <- s_period_rules %>%
     mutate_all(as.character)
   
-  for(i in 1:length(fileNames)) {
-    
-    file_name <- fileNames[i]
-    temp <- read.delim(paste(files, file_name, sep = "/"), stringsAsFactors = FALSE)
-    colnames(temp) <- "period"
-    rule <- temp %>%
-      filter(grepl("=", period, fixed = TRUE)) %>%
-      separate(period, into = c("title","value"),sep = "=")%>%
-      pivot_wider(names_from = title, values_from = value) %>%
-      rename(tvk = Tvk,
-             error_message = ErrorMsg,
-             rule_name = ShortName,
-             rule_group = TestType,
-             start_date = StartDate,
-             end_date = EndDate) %>%
-      mutate(start_date = case_when(nchar(start_date) == 4 ~ paste(gsub("..$", "", start_date),
-                                                                   gsub("^..", "", start_date), sep = "-"),
-                                    TRUE ~ start_date),
-             end_date = case_when(nchar(end_date) == 4 ~ paste(gsub("..$", "", end_date),
-                                                               gsub("^..", "", end_date), sep = "-"),
-                                  TRUE ~ end_date),
-             value = paste(start_date, end_date, sep = " to "),
-             value = gsub(" to NA", " to 12-31", value),
-             value = gsub("NA to ", "01-01 to ", value),
-             value = gsub(" to $", " to 12-31", value)) %>%
-      select(rule_group, rule_name, tvk, error_message, value)
-    
-    f_period_rules <- bind_rows(rule, f_period_rules)
-  }
 }
+
+
+write.csv(wy_period_rules, paste(file_location, "/rules_as_csv/", org, "/", scheme, "/period_within_year.csv", sep = ""), na = "", row.names = FALSE)
+
 
 ### Additional Rules #####
 
-files <- paste(rule_folder, "additional", sep = "/") 
+files <- paste(org, scheme, "additional", sep = "/") 
 fileNames <- list.files(files)
 if(length(fileNames) != 0) {
-additional_rules <- data.frame(matrix(ncol = 5, nrow = 0))
+additional_rules <- data.frame(matrix(ncol = 1, nrow = 0))
 
-colnames(additional_rules) <- c("rule_group",
-                         "rule_name",
-                         "tvk",
-                         "error_message",
-                         "value")
+colnames(additional_rules) <- c("tvk")
 additional_rules <- additional_rules %>%
   mutate_all(as.character)
 
@@ -208,110 +227,70 @@ for(i in 1:length(fileNames)) {
   file_name <- fileNames[i]
   temp <- read.delim(paste(files, file_name, sep = "/"), stringsAsFactors = FALSE)
   colnames(temp) <- "additional"
-  metadata <- temp %>%
-    filter(grepl("=", additional, fixed = TRUE)) %>%
-    separate(additional, into = c("title","value"),sep = "=")%>%
-    pivot_wider(names_from = title, values_from = value) %>%
-    rename(error_message = ErrorMsg,
-           rule_name = ShortName,
-           rule_group = TestType) %>%
-    select(rule_group, rule_name, error_message)
 
   values <- temp %>%
     filter(!grepl("=", additional, fixed = TRUE),
            !grepl("[", additional, fixed = TRUE)) %>%
-    separate(additional, into = c("tvk","value_code"),sep = ",")
-  value_meaning <- temp %>%
-    filter(grepl("=", additional, fixed = TRUE))%>%
-    separate(additional, into = c("value_code","value"),sep = "=") %>%
-    filter(value_code %in% unique(values$value_code))
-  values <- left_join(values, value_meaning, by = "value_code") %>%
-    mutate(value = paste(value_code, value, sep = ": ")) %>%
-    select(-value_code)
+    separate(additional, into = c("tvk","value_code"),sep = ",") 
   
-  rule <- bind_cols(metadata, values)
-  additional_rules <- bind_rows(rule, additional_rules)
+  additional_rules <- bind_rows(values, additional_rules)
 }
+
+additional_rules <- right_join(uksi, additional_rules, by = "tvk") 
+
+} else {
+  
+  additional_rules <- data.frame(matrix(ncol = 3, nrow = 0))
+  
+  colnames(additional_rules) <- c("taxon",
+                                  "tvk",
+                                  "value_code")
+  additional_rules <- additional_rules %>%
+    mutate_all(as.character)
+  
 }
+
+write.csv(additional_rules, paste(file_location, "/rules_as_csv/", org, "/", scheme, "/additional.csv", sep = ""), na = "", row.names = FALSE)
+
+
 ### ID Difficulty #####
 
-files <- id_folder
-fileNames <- list.files(id_folder)
+fileNames <- list.files(org, pattern = paste(scheme, ".+", "txt$", sep = "")) 
 if(length(fileNames) != 0) {
-id_rules <- data.frame(matrix(ncol = 5, nrow = 0))
+id_rules <- data.frame(matrix(ncol = 1, nrow = 0))
 
-colnames(id_rules) <- c("rule_group",
-                                "rule_name",
-                                "tvk",
-                                "error_message",
-                                "value")
+colnames(id_rules) <- c("tvk")
 id_rules <- id_rules %>%
   mutate_all(as.character)
 
 for(i in 1:length(fileNames)) {
   
   file_name <- fileNames[i]
-  temp <- read.delim(paste(files, file_name, sep = "/"), stringsAsFactors = FALSE)
+  temp <- read.delim(paste(org, file_name, sep = "/"), stringsAsFactors = FALSE)
   colnames(temp) <- "id"
-  metadata <- temp %>%
-    filter(grepl("=", id, fixed = TRUE)) %>%
-    separate(id, into = c("title","value"),sep = "=")%>%
-    pivot_wider(names_from = title, values_from = value) %>%
-    rename(rule_group = TestType) %>%
-    select(rule_group)
   
   values <- temp %>%
     filter(grepl("=", id, fixed = TRUE),
            !grepl("[", id, fixed = TRUE)) %>%
     separate(id, into = c("tvk","value_code"),sep = "=") %>%
     filter(grepl("^...SYS", tvk))
-  value_meaning <- temp %>%
-    filter(grepl("=", id, fixed = TRUE))%>%
-    separate(id, into = c("value_code","value"),sep = "=") %>%
-    filter(nchar(value_code) == 1,
-            value_code %in% unique(values$value_code))
-  values <- left_join(values, value_meaning, by = "value_code") %>%
-    mutate(value = paste(value_code, value, sep = ": ")) %>%
-    select(-value_code)
   
-  rule <- bind_cols(metadata, values)
-  id_rules <- bind_rows(rule, id_rules)
-}
+  id_rules <- bind_rows(values, id_rules)
 }
 
-### Combine Datasets #####
+id_rules <- right_join(uksi, id_rules, by = "tvk") 
 
-period_rules <- period_rules %>%
-  rename(recording_period = value) %>%
-  select(tvk, recording_period)
-s_period_rules <- s_period_rules %>%
-  rename(seasonal_recording_period = value) %>%
-  select(tvk, seasonal_recording_period)
-f_period_rules <- f_period_rules %>%
-  rename(flight_period = value) %>%
-  select(tvk, flight_period)
-additional_rules <- additional_rules %>%
-  rename(additional_checks_required = value)%>%
-  select(tvk, additional_checks_required)
-id_rules <- id_rules %>%
-  rename(id_difficulty = value)%>%
-  select(tvk, id_difficulty)
-tenkm_rules <- tenkm_rules %>%
-  rename(grid_10km = value) %>%
-  select(-rule_group)
-all_rules <- period_rules
-all_rules <- full_join(all_rules, s_period_rules, by = "tvk")
-all_rules <- full_join(all_rules, f_period_rules, by = "tvk")
-all_rules <- full_join(all_rules, additional_rules, by = "tvk")
-all_rules <- full_join(all_rules, id_rules, by = "tvk")
-rm(list=ls()[! ls() %in% c("tenkm_rules", "all_rules", "output_name")])
-setwd("C:/Users/robhut/OneDrive - UKCEH/Indicia/record-cleaner-rules/rules_as_csv")
-uksi <- read.csv("uksi_dictionary.csv") %>%
-  select(taxon, preferred_taxon, name_tvk) %>%
-  rename(species = taxon,
-         preferred_species_name = preferred_taxon,
-         tvk = name_tvk)
-all_rules <- right_join(uksi, all_rules, by = "tvk")
 
-write.csv(all_rules, paste(output_name, "Rules - General.csv", sep = " "), na = "", row.names = FALSE)
-write.csv(tenkm_rules, paste(output_name, "Rules - 10km square.csv", sep = " "), na = "", row.names = FALSE)
+} else {
+  
+  id_rules <- data.frame(matrix(ncol = 3, nrow = 0))
+  
+  colnames(id_rules) <- c("taxon",
+                          "tvk",
+                          "value_code")
+  id_rules <- id_rules %>%
+    mutate_all(as.character)
+  
+}
+
+write.csv(id_rules, paste(file_location, "/rules_as_csv/", org, "/", scheme, "_id_difficulty.csv", sep = ""), na = "", row.names = FALSE)
