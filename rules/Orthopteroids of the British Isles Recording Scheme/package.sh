27fca500eab5e298be5d45b317e05f78d8f4033d
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2024-03-22 17:26:30 +0000

    Delete rules/Orthopteroids of the British Isles Recording Scheme directory

--- rules/Orthopteroids of the British Isles Recording Scheme/package.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd OBIRS_IDifficulty
-zip -qr ../../../zip/OBIRS/OBIRS_IDifficulty.zip OBIRS_IDifficulty.txt
-cd ../"OBIRS Grasshopper and Cricket rules"
-zip -qr ../../../zip/OBIRS/"OBIRS Grasshopper and Cricket rules.zip" OBIRS
