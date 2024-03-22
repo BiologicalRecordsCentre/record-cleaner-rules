a9e847ce78da5043b1e74981e1e3e6fba0a8ff02
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2024-03-22 17:26:38 +0000

    Delete rules/Larger Brachycera Recording Scheme directory

--- rules/Larger Brachycera Recording Scheme/package.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd LBRS_IDifficulty
-zip -qr ../../../zip/LBRS/LBRS_IDifficulty.zip LBRS_IDifficulty.txt
-cd ../"LBRS Larger Brachycera rules"
-zip -qr ../../../zip/LBRS/"LBRS Larger Brachycera rules.zip" LBRS
