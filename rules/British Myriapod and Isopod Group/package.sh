6f37ddaf59f274a9b9c3074ba730b997ec47f8f7
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2024-03-22 17:27:44 +0000

    Delete rules/British Myriapod and Isopod Group directory

--- rules/British Myriapod and Isopod Group/package.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd BMIG_IDifficulty
-zip -qr ../../../zip/BMIG/BMIG_IDifficulty.zip BMIG_IDifficulty.txt
-cd ../"BMIG Myriapod and Isopod rules"
-zip -qr ../../../zip/BMIG/"BMIG Myriapod and Isopod rules.zip" BMIG
