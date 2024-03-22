3d7bfa47d8c17caa3e8c6e09585a7de981149cb2
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2024-03-22 17:26:46 +0000

    Delete rules/Ground Beetle Recording Scheme directory

--- rules/Ground Beetle Recording Scheme/package.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd GBRS_IDifficulty
-zip -qr ../../../zip/GBRS/GBRS_IDifficulty.zip GBRS_IDifficulty.txt
-cd ../"GBRS Ground Beetle rules"
-zip -qr ../../../zip/GBRS/"GBRS Ground Beetle rules.zip" GBRS
