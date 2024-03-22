5e623c9a5a6f0f5e8fa013fd4540a8b70fae8ad5
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2024-03-22 17:26:22 +0000

    Delete rules/Plecoptera Recording Scheme directory

--- rules/Plecoptera Recording Scheme/package.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd PRS_IDifficulty
-zip -qr ../../../zip/PRS/PRS_IDifficulty.zip PRS_IDifficulty.txt
-cd ../"PRS Stonefly rules"
-zip -qr ../../../zip/PRS/"PRS Stonefly rules.zip" PRS
