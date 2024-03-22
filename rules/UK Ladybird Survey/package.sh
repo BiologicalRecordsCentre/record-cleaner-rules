eade6b58747cceaddb457fb28effd306badcc80c
robin-hutchinson <95474755+robin-hutchinson@users.noreply.github.com>
2024-03-22 17:25:51 +0000

    Delete rules/UK Ladybird Survey directory

--- rules/UK Ladybird Survey/package.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-cd UKLS_IDifficulty
-zip -qr ../../../zip/UKLS/UKLS_IDifficulty.zip UKLS_IDifficulty.txt
-cd ../"UKLS Ladybird rules"
-zip -qr ../../../zip/UKLS/"UKLS Ladybird rules.zip" UKLS
