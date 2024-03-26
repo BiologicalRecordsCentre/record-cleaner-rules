8bee60963240a106d8f83a67f860a5bb941743e1
robhut <robhut@ceh.ac.uk>
2024-03-26 16:51:58 +0000

    Order rows: DRN/DRN/tenkm/tenkm.csv

--- scripts/schemes.r
+++ scripts/schemes.r
@@ -1,17 +1,89 @@
 # A look up between friendly group names and the path to 
 # their rules.
 schemes = list(
-	Moths = list(
-		CSV_PATH = "Butterfly Conservation",
-		RULES_PATH = "Butterfly Conservation/NMRS Moth rules/NMRS",
-		IDIFF_PATH = "Butterfly Conservation/NMRS_IDifficulty",
-		ORG = "National Moth Recording Scheme"
+	AquaticHeteroptera = list(
+		CSV_PATH = "AHRS"
+		),
+	Auchenorrhyncha = list(
+	  CSV_PATH = "ARS"
+	),
+	Bryophytes = list(
+	  CSV_PATH = "BBS"
+	),
+	Butterflies = list(
+		CSV_PATH = "BC"
+	),
+	Lichen = list(
+	  CSV_PATH = "BLS"
+	),
+	MyriapodsAndIsopods = list(
+	  CSV_PATH = "BMIG"
+	),
+	Algae = list(
+	  CSV_PATH = "BPS"
+	),
+	Plants = list(
+	  CSV_PATH = "BSBI"
+	),
+	Birds = list(
+	  CSV_PATH = "BTO"
+	),
+  BeesWaspsAndAnts = list(
+	  CSV_PATH = "BWARS"
+	),
+	Cladocera = list(
+	  CSV_PATH = "CIG"
+	),
+	Chloropidae = list(
+	  CSV_PATH = "DF"
 	),
 	Odonata = list(
-		CSV_PATH = "Dragonfly Recording Scheme",
-		RULES_PATH = "Dragonfly Recording Scheme/DRN Odonata Rules/DRN",
-		IDIFF_PATH = "Dragonfly Recording Scheme/DRN_IDifficulty",
-		ORGANISATION = "Dragonfly Recording Network"
+	  CSV_PATH = "DRN"
+	),
+	Mayflies = list(
+	  CSV_PATH = "ERS"
+	),
+	GroundBeetles = list(
+	  CSV_PATH = "GBRS"
+	),
+	Gelechiids = list(
+	  CSV_PATH = "GRS"
+	),
+	LargeBrachycera = list(
+	  CSV_PATH = "LBRS"
+	),
+	Marine = list(
+	  CSV_PATH = "MBA"
+	),
+	Mammals = list(
+	  CSV_PATH = "MS"
+	),
+	Bats = list(
+	  CSV_PATH = "MSB"
+	),
+	AmphibiansAndReptiles = list(
+	  CSV_PATH = "NARRS"
+	),
+	Test = list(
+	  CSV_PATH = "NBNT"
+	),
+	Moths = list(
+	  CSV_PATH = "NMRS"
+	),
+	Orthopteroids = list(
+	  CSV_PATH = "OBIRS"
+	),
+	Stoneflies = list(
+	  CSV_PATH = "PRS"
+	),
+	TerrestrialHeteroptera = list(
+	  CSV_PATH = "THRS"
+	),
+	Caddisflies = list(
+	  CSV_PATH = "TRS"
+	),
+	Ladybirds = list(
+	  CSV_PATH = "UKLS"
 	)
 )
 
