297938523caa0b28a80906e100d1cac1f3dcfef6
robhut <robhut@ceh.ac.uk>
2024-04-09 12:22:21 +0100

    Add remaining files

---
+++ scripts/reformat_csvs.R
@@ -0,0 +1,62 @@
+library(gert)
+library(tidyverse)
+file_location <- "C:/Users/robhut/OneDrive - UKCEH/record-cleaner-rules/rules_as_csv"
+
+folders <- as.data.frame(list.dirs(file_location))
+
+colnames(folders) <- "folders"
+
+folders <- folders %>%
+  filter(folders != file_location) %>%
+  mutate(folders = gsub(paste(file_location, "/", sep = ""), "", folders)) %>%
+  filter(grepl("/", folders, fixed = TRUE))
+
+folders <- unique(folders$folders)
+
+setwd(file_location)
+
+for(k in 1:length(folders)) {
+  
+  folder <- folders[k]
+  print(folder)
+  
+  text_files <- list.files(path = folder, pattern = "csv")
+  
+  if(length(text_files) == 0) {next}
+  
+  for(i in 1:length(text_files)){
+    
+    
+    file <- text_files[i]
+    print(file)
+    df <- read.csv(paste(folder, file, sep = "/"))
+    
+    if("value_code" %in% colnames(df)) {
+      
+
+      df <- df %>%
+        rename(code = value_code)
+      
+    }
+    
+    
+    if(length(which(colnames(df) %in% c("ErrorMsg", "text"))) != 0) {
+      
+
+      write.csv(df, paste(folder, file, sep = "/"), row.names = FALSE, na = "", quote = c(which(colnames(df) %in% c("ErrorMsg", "text"))))
+                
+      
+    } else {
+      
+      write.csv(df, paste(folder, file, sep = "/"), row.names = FALSE, na = "", quote = FALSE)
+                
+    }
+    
+    git_add(paste(folder, file, sep = "/"))
+    
+  }
+  
+  git_commit_all(paste("Remove quotations", folder, sep = ": "))
+  git_push()
+}
+
