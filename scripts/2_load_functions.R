177f12c6cea9a1c83186f9abc2603b52901a14be
Jim Bacon <jame2@ceh.ac.uk>
2026-01-12 15:23:36 +0000

    Delete old style rules etc
    All rules have now migrated to the csv file format.
    Old record cleaner is no longer supported and scripts
    for converting between formats are no longer needed.

--- scripts/2_load_functions.R
+++
@@ -1,8 +0,0 @@
-source("scripts/1_update_schemes.R")
-schemes <- readRDS("scripts/schemes.RData")
-source("scripts/species.r")
-source("scripts/tenkm.r")
-source("scripts/idifficulty.r")
-source("scripts/period.r")
-source("scripts/periodwithinyear.r")
-
